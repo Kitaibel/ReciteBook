7ea26bc38ec3d2403f388f8c497d59b73d3bca5e
Kitaibel <386964993@qq.com>
2019-03-12 21:27:50 +0800

    完成了登录的界面设计

--- header/header.js
+++ header/header.js
@@ -8,7 +8,7 @@ for(var k=1;k<=10;k++)
 //顶部标题
 var header_tips = "Meitnerium's English Word Book";
 //一级菜单内容和链接
-classes[1] = new first_class("首页","#");
+classes[1] = new first_class("首页","http://liuzhiyuan.online");
 classes[2] = new first_class("单词","#");
 classes[3] = new first_class("读书","#");
 classes[4] = new first_class("个人中心","#");
