b5b2a814c33efdec4848d8050cb30f357fc06310
Kitaibel <386964993@qq.com>
2019-03-30 16:20:47 +0800

    记单词页面已经实现动态

--- word.php
+++ word.php
@@ -1,9 +1,5 @@
- <html>
- <body>
-
-<br>
-
  <?php 
+ header('Content-Type: text/html; charset=UTF-8'); 
 $servername = "localhost"; 
 $username = "ReciteBook"; 
 $password = "uestcXA#3008"; 
@@ -18,17 +14,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error); 
 } 
 //echo "Connected successfully<br>";
+//$posttest = $_POST["userpid"];
 
 $sql = "SELECT * FROM userwords where UserPID='" . $_POST["userpid"] . "'";
 $result = $conn->query($sql);
 
 $row = mysqli_fetch_assoc($result);
 
-echo "<div>111111</div>";
+$userlevel1 = $row["UserLevel1"];
+$userlevel2 = $row["UserLevel2"];
+$userlevel3 = $row["UserLevel3"];
+$userlevel4 = $row["UserLevel4"];
+$userlevel5 = $row["UserLevel5"];
+
+$ajax_result = json_encode(array('wordlevel1' => $userlevel1, 'wordlevel2' => $userlevel2, 'wordlevel3' => $userlevel3, 'wordlevel4' => $userlevel4, 'wordlevel5' => $userlevel5));
+
+echo $ajax_result;
+// echo 1;
 
 ?>
-
-
-
- </body>
- </html> 
--- word/index.js
+++ word/index.js
@@ -1,5 +1,24 @@
+var words_level = {
+    words_level1: null,
+    words_level2: null,
+    words_level3: null,
+    words_level4: null,
+    words_level5: null,
+}
+
+var vword;
+
 onload = function() {
     header();
+
+    vword = new Vue({
+        el: '#main',
+        data: {
+            word: null,
+            mean: null,
+            level: 1
+        }
+    });
 
     function getCookie(name)
     {
@@ -13,12 +32,71 @@ onload = function() {
     if(getCookie("UserPID")==0 || getCookie("UserPID")==null) {
         window.location.href = "../login/index.html";
     }
+    else {
+        $.ajax({
+            type: 'POST',
+            url: '../word.php',
+            async: false,
+            data: {
+                userpid: getCookie("UserPID")
+            },
+            dataType: "json",
+            success: function(data) {
+                words_level.words_level1 = JSON.parse(data.wordlevel1);
+                words_level.words_level2 = JSON.parse(data.wordlevel2);
+                words_level.words_level3 = JSON.parse(data.wordlevel3);
+                words_level.words_level4 = JSON.parse(data.wordlevel4);
+                words_level.words_level5 = JSON.parse(data.wordlevel5);
+                console.log("数据加载完成");
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown){//请求失败时调用此函数  
+                console.log(XMLHttpRequest.status);  
+                console.log(XMLHttpRequest.readyState);  
+                console.log(textStatus);    
+                alert("出错了，请稍后再试，可以联系作者QQ386964993提交错误，万分感谢");                          
+            }
+        });
 
-    $.ajax({
-        type: 'POST',
-        url: '../word.php',
-        data: {userpid: getCookie("UserPID")}
-    });
-
-
+        if(words_level.words_level1 == null) {
+            vword.level++;
+            if(words_level.words_level2 == null) {
+                vword.level++;
+                if(words_level.words_level3 == null) {
+                    vword.level++;
+                    if(words_level.words_level4 == null) {
+                        vword.level++;
+                        if(words_level.words_level5 == null) {
+                            //自动跳转添加单词
+                            window.location.href = "../addword/index.html";
+                        }
+                        else {
+                            vword.word = Object.keys(words_level.words_level5)[0];
+                            vword.mean = decodeURI(words_level.words_level5[vword.word]);
+                        }
+                    }
+                    else {
+                        vword.word = Object.keys(words_level.words_level4)[0];
+                        vword.mean = decodeURI(words_level.words_level5[vword.word]);
+                    }
+                }
+                else {
+                    vword.word = Object.keys(words_level.words_level3)[0];
+                    vword.mean = decodeURI(words_level.words_level3[vword.word]);
+                }
+            }
+            else {
+                vword.word = Object.keys(words_level.words_level2)[0];
+                vword.mean = decodeURI(words_level.words_level2[vword.word]);
+            }
+        }
+        else {
+            vword.word = Object.keys(words_level.words_level1)[0];
+            vword.mean = decodeURI(words_level.words_level1[vword.word]);
+        }
+    }
+    
+    
 }
+
+
+//decodeURI();
