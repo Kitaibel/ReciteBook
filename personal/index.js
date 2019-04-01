ad7fc0a0a39ce68eeca466c7dcb51f44ae4a9449
Kitaibel <386964993@qq.com>
2019-04-01 15:28:11 +0800

    进行了部分逻辑优化

--- personal/index.js
+++ personal/index.js
@@ -1,14 +1,13 @@
+function getCookie(name) {
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+
 onload = function() {
    header();
-
-   function getCookie(name)
-   {
-      var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-      if(arr=document.cookie.match(reg))
-         return unescape(arr[2]);
-      else
-         return null;
-   }
 
    if(getCookie("UserPID") == 0 || getCookie("UserPID") == null) {
       window.location.href = "../login/index.html";
